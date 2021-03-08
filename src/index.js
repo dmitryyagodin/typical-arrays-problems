9ccb030d973338f8fbd4b70a5d3e2a7570124d58
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2021-03-08 11:57:07 +0200

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = [0]) {
+    
+  return array ? array.reduce((a, b) => b > a ? a : b, 0) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = [0]) {
+  return array ? array.reduce((a, b) => b > a ? b : a, 0) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = [0]) {
+  return array.length > 0 ? array.reduce((a, b) => (a + b), 0) / array.length : 0;
 }
